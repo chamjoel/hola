d927ab79ce59007df2fa8047d72a485b46ed6dfb
Joel-Chambergo <chambergo.joel@iisgalvanimi.edu.it>
2019-12-11 12:52:10 +0000

    punto2

--- hablahispana/src/app/container/container.component.ts
+++ hablahispana/src/app/container/container.component.ts
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./container.component.css']
 })
 export class ContainerComponent implements OnInit {
+ cantiere; String;
 
-  constructor() { }
+  constructor() {
+this.cantiere=['macchina1', 'macchina2','macchina3','macchina4','macchina5','macchina6','macchina7','macchina8','macchina9','macchina10']
+   }
 
   ngOnInit() {
   }
